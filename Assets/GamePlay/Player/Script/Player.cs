5196fb6c3aafd4938a2ed04f334b5d1aa7bed8bd
h0anganhcute <h0anganhcute66@gmail.com>
2025-12-11 21:56:35 +0700

    ccc

--- Assets/GamePlay/Player/Script/Player.cs
+++ Assets/GamePlay/Player/Script/Player.cs
@@ -4,34 +4,55 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Animator ani;
-
-
+    [SerializeField] GameObject Bullet;   // Kéo prefab Bullet vào đây
+    [SerializeField] float bulletSpeed = 10f;
+    [SerializeField] GameObject Checkpoint;
     void Start()
     {
-
         ani = GetComponent<Animator>();
-
     }
 
     void Update()
     {
-        // Nếu chết thì không cho làm gì nữa
-
-
-
         StartCoroutine(Dropping());
+        StartCoroutine(Attackk());
+
+        // Bắn đạn khi click chuột trái
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
-
-
 
     public IEnumerator Dropping()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            ani.SetBool("Drop",true);
+            ani.SetBool("Drop", true);
             yield return new WaitForSeconds(0.3f);
             ani.SetBool("Drop", false);
         }
     }
 
+    public IEnumerator Attackk()
+    {
+        if (Input.GetMouseButtonDown(0)) // 0 = chuột trái
+        {
+            ani.SetBool("Attack", true);
+            yield return new WaitForSeconds(1.5f);
+            ani.SetBool("Attack", false);
+        }
+    }
+
+    void Shoot()
+    {
+        // Tạo viên đạn tại vị trí Checkpoint, hướng theo forward của nó
+        GameObject bullet = Instantiate(Bullet, Checkpoint.transform.position, Quaternion.identity);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.useGravity = false; // nếu không muốn đạn bị rơi
+            rb.linearVelocity = Checkpoint.transform.forward * bulletSpeed;
+        }
+    }
 }
