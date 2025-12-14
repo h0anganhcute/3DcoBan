ab02a9dea24e92d5a5c8ea80e1e4dffbeb2bd132
duyphan-atk <ptanduy2512@gmail.com>
2025-12-14 22:40:37 +0700

    setmenu

--- Assets/menu/MainMenu.cs
+++ Assets/menu/MainMenu.cs
@@ -1,11 +1,56 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("duyScenes");
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu"); // nhớ add scene vào Build Settings
+    }
+
+  
     public void StartGame()
     {
-        SceneManager.LoadScene("Map_v1");
+        SceneManager.LoadScene("duyScenes");
     }
     public void QuitGame()
     {
